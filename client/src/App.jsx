a1d398dda67eef916f677abc4bbe84543a003855
Venancio Caceres <venancio.caceres03@gmail.com>
2023-04-05 21:40:47 -0400

    admin productos parte I

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import ProductForm from './components/ProductForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Introduce your products</h1>
+      <ProductForm/>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
